30908f6701f96d25c02550ee4812846e89a2a505
AzerDime <guyian@pdx.edu>
2022-01-04 20:22:57 -0800

    almost there

--- HW1/src/main.rs
+++ HW1/src/main.rs
@@ -1,8 +1,10 @@
 //!Homework 1 for CS410P - Rust
 // Ian Guy 2022
 
+use std::env;
+
 //Error tool from hints
-fn error() -> !{
+fn error() -> ! {
     eprintln!("modexp: useage: modexp <x> <y> <m>");
     std::process::exit(1);
 }
@@ -12,18 +14,30 @@ fn parsenum(s: &str) -> u64 {
     s.parse().unwrap_or_else(|_| error())
 }
 
-fn main(){
-    let mut numbers = Vec::new();
+fn main() {
+    //let numbers = Vec::new();
+    let args: Vec<String> = env::args().skip(1).collect();
 
-    if numbers.len() < 3 {
+    if args.len() != 3 {
         error();
     }
-    
-    let mut x = parsenum(numbers[0]);
-    let mut y = parsenum(numbers[1]);
-    let mut m = parsenum(numbers[2]);
-
-    if (m == 0) || (u128::from((m-1)*(m-1)) == u128::MAX) {
+    println!("Correct value amount");
+    let mut x = parsenum(&args[0]);
+    let mut y = parsenum(&args[1]);
+    let m = parsenum(&args[2]);
+    let mut ans = 1;
+    if (m == 0) || (u128::from((m - 1) * (m - 1)) == u128::MAX) {
         error();
+    } else if m == 1 {
+        return;
     }
+
+    while y > 0 {
+        if y % 2 == 1 {
+            ans = (ans * x) % m;
+        }
+        y /= 2;
+        x = (x ^ 2) % m;
+    }
+    println!("{}", ans);
 }
