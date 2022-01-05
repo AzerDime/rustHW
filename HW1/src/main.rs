b040b5cdaa0bfb6a64eee26bc78e84c96e0430bc
AzerDime <guyian@pdx.edu>
2022-01-04 18:10:40 -0800

    error and parsenum added

--- HW1/src/main.rs
+++ HW1/src/main.rs
@@ -1,5 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+//!Homework 1 for CS410P - Rust
+// Ian Guy 2022
+
+use std::io;
+
+//Error tool from hints
+fn error() -> !{
+    eprintln!("modexp: useage: modexp <x> <y> <m>");
+    std::process::exit(1);
 }
 
+//Parse tool from hints
+fn parsenum(s: &str) -> u64 {
+    s.parse().unwrap_or_else(|_| error())
+}
 
+fn main(){
+    //using some code from the rust-cli github page for command line inputs
+    let args = Cli::from_args();
+    
+}
