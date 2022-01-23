bb3fa1e0ec5bbb1c0cb2120654d73afce7cd9aa0
AzerDime <guyian@pdx.edu>
2022-01-22 20:27:26 -0800

    added a test. will look into compacting nth uppercase

--- HW3/tests/tests.rs
+++ HW3/tests/tests.rs
@@ -2,7 +2,8 @@ use kwindex::*;
 
 #[test]
 fn test_kwindex() {
-    let kwindex = KWIndex::new().extend_from_text("hello world.");
-    assert_eq!(1, kwindex.count_matches("hello"));
-    assert_eq!(2, kwindex.len());
+    let kwindex = KWIndex::new().extend_from_text("I am THE WALRUS");
+    assert_eq!(1, kwindex.count_matches("THE"));
+    assert_eq!(4, kwindex.len());
+    assert_eq!(Some("WALRUS"), kwindex.nth_uppercase(2));
 }
